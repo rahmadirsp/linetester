666bc263deff032e4786497ad737f911aa2d3de3
rahmadirsp <rahmadi.rr@gmail.com>
2017-08-27 00:20:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -256,14 +256,6 @@ def handle_beacon(event):
 
 
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-
     # create tmp dir for download content
     make_static_tmp_dir()
-
-    app.run(debug=options.debug, port=options.port)
+    app.run()
